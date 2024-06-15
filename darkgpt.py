f509aeb56073ca7cb2e5cffcfbb8b623a1c24cc0
MBKMADMAX <116323761+MHackerkhan@users.noreply.github.com>
2024-06-15 11:23:29 +0500

    Update gpt.py

--- darkgpt.py
+++ darkgpt.py
@@ -1,15 +1,18 @@
-import requests 
+import requests
+
 base_url = 'http://62.72.6.182:1717'
 dark = f'{base_url}/darkgpt'
 darkbase = f'{base_url}/test_route'
 json_data = {
-        'task': "generator",
-        'program': "python",
-        "prompt": "he'll world",
-        "code": None,
-        'further_description1' : 'none',
-        'further_description2' : 'none'
-         }
-headers = {'X-API-KEY': 'pUC3BEfonGJU5ivGb7FMAr189oVH73rj7m9Its1eg09cDSLMCZAJH-XSSDKPPEsklOc'} 
-o = requests.get(dark, json=json_data, headers=headers)
+    'task': "generator",
+    'program': "python",
+    "prompt": "he'll world",
+    "code": None,
+    'further_description1': 'none',
+    'further_description2': 'none'
+}
+headers = {'X-API-KEY': 'pUC3BEfonGJU5ivGb7FMAr189oVH73rj7m9Its1eg09cDSLMCZAJH-XSSDKPPEsklOc'}
+
+# Change from requests.get to requests.post
+o = requests.post(dark, json=json_data, headers=headers)
 print(o.text)
