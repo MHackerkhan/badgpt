a8273eeb4e58dce8059df1d77478f0c124920182
MBKMADMAX <116323761+MHackerkhan@users.noreply.github.com>
2024-06-15 11:10:15 +0500

    Update darkgpt.py

--- darkgpt.py
+++ darkgpt.py
@@ -6,9 +6,9 @@ json_data = {
         'task': "generator",
         'program': "python",
         "prompt": "he'll world",
-        "code": None ,
+        "code": None,
         'further_description1' : 'none',
-        'further_description2' : 'none',
+        'further_description2' : 'none'
          }
 headers = {'X-API-KEY': 'pUC3BEfonGJU5ivGb7FMAr189oVH73rj7m9Its1eg09cDSLMCZAJH-XSSDKPPEsklOc'} 
 o = requests.get(dark, json=json_data, headers=headers)
